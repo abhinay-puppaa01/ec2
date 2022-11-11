a7aa3a851176c9cbeadd8ba41fbb43e7dc373ed3
abhinay-puppaa01 <110231507+abhinay-puppaa01@users.noreply.github.com>
2022-11-11 15:19:12 +0530

    Update mvariable.tf

--- mvariable.tf
+++ mvariable.tf
@@ -114,7 +114,7 @@ default = "<%= customOptions.subnet_id%>"
 }
 variable "vpc_security_group_ids" {
   description = "vpc_security_group_ids"
-  type        =  string
+  type        =  list
 default = "<%= customOptions.vpc_security_group_ids.encodeAsJson().toString()%>"
 }
 variable "tags" {
