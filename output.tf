8a50a27e25f48dc787daca296dbb5249ba3900da
abhinay-puppaa01 <110231507+abhinay-puppaa01@users.noreply.github.com>
2022-11-16 16:04:03 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -11,6 +11,11 @@ output "ec2_test_public_ip" {
   description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
   value       = try(aws_instance.ec2_test[*].public_ip, "")
 }
+output "ec2_test_private_ip" {
+  description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
+  value       = try(aws_instance.ec2_test[*].private_ip, "")
+}
+
 
 output "ec2_test_tags_all" {
   description = "A map of tags assigned to the resource, including those inherited from the provider default_tags configuration block"
@@ -42,7 +47,7 @@ output "root_block_device" {
   description = "The name of root block device"
   value       = aws_instance.ec2_test[*].root_block_device[0].device_name
 }
-output "root_block_device_encrytion" {
+output "root_block_device_encryption" {
   description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
   value       = aws_instance.ec2_test[*].root_block_device[0].encrypted
 }
