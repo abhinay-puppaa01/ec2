59ed26a84bbf124257bbadf9e486fe14f509dfc5
abhinay-puppaa01 <110231507+abhinay-puppaa01@users.noreply.github.com>
2022-11-21 15:14:21 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -28,7 +28,7 @@ output "private_ip" {
 }
 output "vpc_id" {
   description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
-  value       = try(aws_instance.ec2_tes[*]t.vpc_security_group_ids , "")
+  value       = try(aws_instance.ec2_test[*].vpc_security_group_ids , "")
 }
 
 output "subnet_id" {
