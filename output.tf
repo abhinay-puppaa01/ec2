9d722962ee2a38b3829cf9ae0b31f636a8605134
abhinay-puppaa01 <110231507+abhinay-puppaa01@users.noreply.github.com>
2022-11-21 14:42:59 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,83 +1,108 @@
-output "ec2_id" {
+output "machine_id" {
   description = "The ID of the instance"
-  value       = try(aws_instance.ec2_test[*].id, "")
+  value       = try(aws_instance.ec2_test.id, "")
 }
-output "machine-names" {
-  description = "The ID of the instance"
-  value       = try(aws_instance.ec2_test[*].tags_all.name, "")
-}
-
-output "ec2_test_public_ip" {
-  description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
-  value       = try(aws_instance.ec2_test[*].public_ip, "")
-}
-output "ec2_test_private_ip" {
-  description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
-  value       = try(aws_instance.ec2_test[*].private_ip, "")
-}
-
-
-output "ec2_test_tags_all" {
-  description = "A map of tags assigned to the resource, including those inherited from the provider default_tags configuration block"
-  value       = try(aws_instance.ec2_test[*].tags, {})
-}
-output "primary_network_interface_id" {
-  description = "The ID of the instance's primary network interface"
-  value       = try(aws_instance.ec2_test[*].primary_network_interface_id, "")
-}
-output "ec2_instance_state" {
+output "machine_state" {
   description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
-  value       = aws_instance.ec2_test[*].instance_state
+  value       = aws_instance.ec2_test.instance_state
+}
+output "machine_type" {
+  description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
+  value       = aws_instance.ec2_test.instance_type
 }
 
-output "root_block_device_volume_id" {
+output "iam_role" {
+  description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
+  value       = aws_instance.ec2_test.iam_instance_profile
+}
+
+
+output "image_id" {
+  description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
+  value       = aws_instance.ec2_test.ami
+}
+
+output "private_ip" {
+  description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
+  value       = try(aws_instance.ec2_test.private_ip, "")
+}
+output "vpc_id" {
+  description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
+  value       = try(aws_instance.ec2_test.vpc_security_group_ids , "")
+}
+
+output "subnet_id" {
+  description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
+  value       = try(aws_instance.ec2_test.subnet_id, "")
+}
+
+output "network_interface_id" {
+  description = "The ID of the instance's primary network interface"
+  value       = try(aws_instance.ec2_test.primary_network_interface_id, "")
+}
+
+
+output "volume_root_id" {
   description = "The volume Id of root block device"
-  value       = aws_instance.ec2_test[*].root_block_device[0].volume_id
+  value       = aws_instance.ec2_test.root_block_device[0].volume_id
 }
 
 output "root_block_device_volume_size" {
   description = "The volume size of root block device"
-  value       = aws_instance.ec2_test[*].root_block_device[0].volume_size
+  value       = aws_instance.ec2_test.root_block_device[0].volume_size
 }
-output "root_block_device_volume_type" {
+output "volume_root_device_type" {
   description = "The volume type of root block device"
-  value       = aws_instance.ec2_test[*].root_block_device[0].volume_type
+  value       = aws_instance.ec2_test.root_block_device[0].volume_type
 }
-output "root_block_device" {
+output "volume_root_device_name" {
   description = "The name of root block device"
-  value       = aws_instance.ec2_test[*].root_block_device[0].device_name
+  value       = aws_instance.ec2_test.root_block_device[0].device_name
 }
-output "root_block_device_encryption" {
+output "volume_encrypted" {
   description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
-  value       = aws_instance.ec2_test[*].root_block_device[0].encrypted
+  value       = aws_instance.ec2_test.root_block_device[0].encrypted
 }
+output "ebs_optimized" {
+  description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
+  value       = aws_instance.ec2_test.ebs_optimized
+}
+output "availability_zone" {
+  description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
+  value       = aws_instance.ec2_test.availability_zone
+}
+output "dedicated_tenency" {
+  description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
+  value       = aws_instance.ec2_test.tenancy
+}
+output "dedicated_hostid" {
+  description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
+  value       = aws_instance.ec2_test.host_id
+}
+output "resource_name" {
+  description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
+  value       = aws_instance.ec2_test.tags.Name
+}
+
+
+
+
 /*
+output "ec2_test_tags_all" {
+  description = "A map of tags assigned to the resource, including those inherited from the provider default_tags configuration block"
+  value       = try(aws_instance.ec2_test.tags, {})
+}
 output "ebs_block_device" {
   description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
     value       = aws_instance.ec2_test.ebs_block_device
     
-}*/
+}
 output "ebs_block_device" {
   description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
-  value       = aws_instance.ec2_test[*].ebs_block_device
+  value       = aws_instance.ec2_test.ebs_block_device[*].device_name
 
-}
-
-output "ebs_block_device1" {
-  description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
-  value       = local.ebs_volumes
-
-}
-/*output "ebs_block_device2" {
-  description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
- 
-value       = [for i in aws_instance.ec2_test.ebs_block_device : {
-      device_name    = aws_instance.ec2_test.ebs_block_device[*].device_name
-      volume_size    = aws_instance.ec2_test.ebs_block_device[*].volume_size
-      volume_type = aws_instance.ec2_test.ebs_block_device[*].volume_type
-      iops = aws_instance.ec2_test.ebs_block_device[*].iops
-    }]
 }*/
+
 
 
 
